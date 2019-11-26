279f0c0bf94bb09f51f27ae06e9ec105bf15f52e
amitnh <amitnh@gmail.com>
2019-11-26 17:06:25 +0200

    cmakeLists update, main is running now

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 #include "../include/Session.h"
-#include <nlohmann/json.hpp>
 using namespace std;
 
 int main(int argc, char** argv){
+    cout <<"hello amit"<<endl;
 	if(argc!=2)
 	{
 		cout << "usage splflix input_file" << endl;
 		return 0;
 	}
-	cout <<"hello amit"<<endl;
-	Session s(argv[1]);
-	s.start();
+	//Session s(argv[1]);
+	//s.start();
 	return 0;
 }
